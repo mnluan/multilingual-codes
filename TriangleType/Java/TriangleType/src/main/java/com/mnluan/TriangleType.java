e0c40633336021756a44fa4dc8d024a842ae3564
mnluan <luanlm@live.com>
2023-08-03 20:17:37 -0300

    Now it is possible to identify the types of triangles

--- TriangleType/Java/TriangleType/src/main/java/com/mnluan/TriangleType.java
+++ TriangleType/Java/TriangleType/src/main/java/com/mnluan/TriangleType.java
@@ -19,7 +19,7 @@ public class TriangleType {
             System.out.println("The sizes of the sides should be positive.");
         }else{
             if(isTriangle(x,y,z)){
-                System.out.println("Yeah, It's a triangle!");
+                System.out.println("Yeah, It's a " + calcFormat(x,y,z) +" triangle!");
             }else{
                 System.out.println("Nope, It's not a triangle!");
             }
@@ -32,4 +32,21 @@ public class TriangleType {
         return ((x + y > z) && (x + z > y) && (y + z > x));
     }
 
+    private static String calcFormat(double x, double y, double z){
+        if (x == y && y == z){
+            return "Equilateral";
+        }else if (x == y || x == z || y == z){
+            return "Isosceles";
+        }else{
+            double biggerSide = Math.max(Math.max(x,y), z);
+            double sumSquaresMinorSides = (Math.pow(x, 2) + Math.pow(y, 2) + Math.pow(z, 2) - Math.pow(biggerSide, 2));
+            if (sumSquaresMinorSides > Math.pow(biggerSide, 2)){
+                return "Acute";
+            } else if (sumSquaresMinorSides < Math.pow(biggerSide, 2)) {
+                return "Obtuse";
+            } else {
+                return "Right";
+            }
+        }
+    }
 }
